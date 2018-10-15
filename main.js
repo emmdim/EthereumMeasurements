c7a14e9006227779ba7f85cbdf0dc6f1fc386786
manos <manos@dync-42-252.ac.upc.edu>
2018-10-15 21:49:46 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,72 @@
+const web3 = require('./web3.js');
+const fs = require('fs');
+const json2csv = require('json2csv').parse;
+
+const acc2 = '0xC202251cabC1393f8Face351057666c43f9a432A' //client
+
+const REPETITIONS = 100;
+const ETHER = 1;
+
+let times = [];
+let account;
+
+const getAccount = async () => {
+  const accounts = await web3.eth.getAccounts();
+  account = accounts[0];
+  console.log('Working from account ', account);
+};
+
+let isFinished = times => {
+    return times.filter(a => typeof(a['completionTime']) === 'undefined');
+}
+
+let c = getAccount().then( async function() {
+  let nonce = await web3.eth.getTransactionCount(account);
+  //console.log(nonce);
+  sendTransactions(nonce, REPETITIONS, ETHER);
+});
+
+function sendTransactions(nonce, repetitions, etherVal) {
+  for (let id = 0; id <repetitions; id++) {
+    nonce += 1;
+    times[id] = {};
+    times[id]['id'] = id;
+    times[id]['start'] = Date.now();
+    web3.eth.sendTransaction({
+      from: account,
+      to: acc2,
+      //nonce: nonce, // increment the nonce for every transaction
+      value: web3.utils.toWei(String(1)),
+      gas: 3000000
+    }).on('confirmation', function(confirmationNumber, receipt){
+        if (confirmationNumber==0) {
+          times[id]['mindTime']=Date.now()-times[id]['start'];
+          console.log(times[id]);
+        }
+        if (confirmationNumber==12) {
+          times[id]['completionTime']=Date.now()-times[id]['start'];
+          console.log('ID: '+id);
+          console.log(times[id]);
+          if (isFinished(times).length == 0) {
+            console.log('Process completed');
+            try{
+              let jsonContent = JSON.stringify(times);
+              fs.writeFileSync("mennan.json", jsonContent, 'utf8');
+              console.log("JSON file has been saved.");
+            } catch (err){
+              console.error(err);
+            }
+            try {
+              const csv = json2csv(times, ['id','start', 'deploymentTime', 'mindTime', 'completionTime']);
+              fs.writeFileSync('mennan_'+String(REPETITIONS)+'.csv', csv, 'utf8');
+              console.log("CSV file has been saved.");
+              process.exit(0)
+            } catch (err) {
+              console.error(err);
+            }
+          }
+      };
+    });
+    times[id]['deploymentTime'] = Date.now()-times[id]['start'];
+  }
+}
---
+++ web3.js
@@ -0,0 +1,27 @@
+const Web3 = require('web3');
+
+let web3;
+
+if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
+  // We are in the browser and metamask is running.
+  //web3 = new Web3(window.web3.currentProvider);
+  // Override this and set the same provider
+  //const provider = new Web3.providers.WebsocketProvider(
+  //  'ws://10.1.24.69:8503'
+  //);
+  //web3 = new Web3(provider);
+  web3 = new Web3(window.web3.currentProvider);
+} else {
+  // We are on the server *OR* the user is not running metamask
+  ///*
+  const provider = new Web3.providers.WebsocketProvider(
+    'ws://10.1.26.10:8503'
+  );
+  //*/
+  /* HTTP for deploying and WS for the rest of the process (oracle needs WS)
+  const provider = new Web3.providers.HttpProvider('http://10.1.26.10:8502');
+  */
+  web3 = new Web3(provider);
+}
+
+module.exports = web3;
